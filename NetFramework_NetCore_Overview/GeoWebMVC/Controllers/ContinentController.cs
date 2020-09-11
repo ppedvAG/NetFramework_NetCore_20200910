66bbe89e86b5b9a2b0727c3d699b7a5bba28db1e
Kevin Winter <KevinW@ppedv.de>
2020-09-11 13:30:54 +0200

    ASP.MVC Generated Views

---
+++ NetFramework_NetCore_Overview/GeoWebMVC/Controllers/ContinentController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GeoDB.SharedLibrary.Models;
+using GeoDBWebAPI.Data;
+
+namespace GeoWebMVC.Controllers
+{
+    public class ContinentController : Controller
+    {
+        private readonly GeoDBContext _context;
+
+        public ContinentController(GeoDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Continent
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Continents.ToListAsync());
+        }
+
+        // GET: Continent/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var continents = await _context.Continents
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (continents == null)
+            {
+                return NotFound();
+            }
+
+            return View(continents);
+        }
+
+        // GET: Continent/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Continent/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] Continents continents)
+        {
+            if (ModelState.IsValid)
+            {
+                continents.Id = Guid.NewGuid();
+                _context.Add(continents);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(continents);
+        }
+
+        // GET: Continent/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var continents = await _context.Continents.FindAsync(id);
+            if (continents == null)
+            {
+                return NotFound();
+            }
+            return View(continents);
+        }
+
+        // POST: Continent/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Name")] Continents continents)
+        {
+            if (id != continents.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(continents);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ContinentsExists(continents.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(continents);
+        }
+
+        // GET: Continent/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var continents = await _context.Continents
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (continents == null)
+            {
+                return NotFound();
+            }
+
+            return View(continents);
+        }
+
+        // POST: Continent/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var continents = await _context.Continents.FindAsync(id);
+            _context.Continents.Remove(continents);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ContinentsExists(Guid id)
+        {
+            return _context.Continents.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ NetFramework_NetCore_Overview/GeoWebMVC/Data/GeoDBContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using GeoDB.SharedLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace GeoDBWebAPI.Data
+{
+    public partial class GeoDBContext : DbContext
+    {
+        public GeoDBContext()
+        {
+        }
+
+        public GeoDBContext(DbContextOptions<GeoDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Continents> Continents { get; set; }
+        public virtual DbSet<Countries> Countries { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=SURFACE-KW4;Initial Catalog=GeoDB;Integrated Security=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Continents>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Countries>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Name).HasMaxLength(80);
+
+                entity.HasOne(d => d.Continent)
+                    .WithMany(p => p.Countries)
+                    .HasForeignKey(d => d.ContinentId)
+                    .HasConstraintName("FK_Countries_Continents");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
